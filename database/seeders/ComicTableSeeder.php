a4eb11c58a4993ef354588f1f03d67242a6e66e3
“GiuseppeM22” <giu.mongelli97@gmail.com>
2023-07-04 16:39:40 +0200

    add seeder non funzionante

---
+++ database/seeders/ComicTableSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Comic;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ComicTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $fumetti = config('comics');
+
+        foreach ($fumetti as $fumetto) {
+
+           $newFumetto = new Comic();
+           $newFumetto->title = $fumetto["title"];
+           $newFumetto->description = $fumetto["description"];
+           $newFumetto->thumb = $fumetto["thumb"];
+           $newFumetto->price = $fumetto["price"];
+           $newFumetto->series = $fumetto["series"];
+           $newFumetto->sale_date = $fumetto["sale_date"];
+           $newFumetto->type = $fumetto["type"];
+           $newFumetto->artists = $fumetto["artists"];
+           $newFumetto->writers = $fumetto["writers"];
+
+           $newFumetto->save();
+        }
+    }
+}
